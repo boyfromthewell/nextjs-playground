8682f72ff3a3d3c2d5b55d380a3a783ece90622d
boyfromthewell <tnsdyd10@naver.com>
2024-07-24 10:42:27 +0900

    feat: 프로젝트 세팅 및 스타일 컴포넌트 적용

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+  button {
+    cursor: pointer;
+    background-color: inherit;
+  }
+  a {
+    text-decoration:none;
+    color:inherit;
+  }
+`
+export default GlobalStyle
